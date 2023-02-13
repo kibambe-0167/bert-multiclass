233f17c76443e9f8dec354f99269a69233f8ee69
kibambe-0167 <kibambe.0167@gmail.com>
2023-02-13 10:10:57 +0200

    trained model, with bad data and low accuracy

--- lstm.py
+++ lstm.py
@@ -4,15 +4,17 @@ from keras.layers import Embedding, Bidirectional, Dense, Activation, LSTM
 from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 import pandas as pd
-
 
 _NUM_WORDS = 80000
 _INPUT_LEN = 500
 _BATCH_SIZE = 256
-_EPOCH = 4
+_EPOCH = 10
 
-data = pd.DataFrame("./clean_data/shona.csv" )
+data = pd.read_csv("./clean_data/shona.csv" )
+
+print(data.head())
 
 X_train, X_test, y_train, y_test = train_test_split(
   data['shona'],
@@ -28,10 +30,7 @@ X_train, X_val, y_train, y_val = train_test_split(
   test_size=.25,
   stratify=y_train,
   random_state=43
-  )
-
-# view data.
-print(X_train.head(5))
+)
 
 # reset index of data and drop them.
 X_train.reset_index(drop=True,inplace=True)
@@ -40,12 +39,17 @@ X_test.reset_index(drop=True,inplace=True)
 y_train.reset_index(drop=True,inplace=True)
 y_val.reset_index(drop=True,inplace=True)
 y_test.reset_index(drop=True,inplace=True)
-
+# print("\n\n",X_train.head())
 
 # create tokenizer object and set number of words for tokenizer.
 # fit train text on class object.
 text_tok_obj = Tokenizer(num_words=_NUM_WORDS)
-text_tok_obj.fit_on_texts(X_train['shona'].astype(str) )
+text_tok_obj.fit_on_texts(X_train.astype(str) )
+
+# 
+X_train = text_tok_obj.texts_to_sequences(X_train.astype(str))
+X_val = text_tok_obj.texts_to_sequences(X_val.astype(str))
+X_test= text_tok_obj.texts_to_sequences(X_test.astype(str))
 
 # tokenize the text .
 X_train_tok = pad_sequences( X_train, maxlen=_INPUT_LEN, padding='post' )
@@ -56,7 +60,7 @@ X_test_tok = pad_sequences( X_test, maxlen=_INPUT_LEN, padding='post' )
 
 # make model and add layers to it.
 model = Sequential()
-model.add(( Embedding(_NUM_WORDS, 128, input_length=_INPUT_LEN)))
+model.add((Embedding(_NUM_WORDS, 128, input_length=_INPUT_LEN)))
 model.add(Bidirectional(LSTM(128)))
 model.add(Dense(128, name='FC1'))
 model.add(Activation('relu'))
@@ -64,7 +68,7 @@ model.add(Dense(1, name='out_layer'))
 model.add(Activation('sigmoid'))
 
 # set loss, optimizer and metric to use.
-model.compile( loss='crossentropy', optimizer='adam',metrics=['accuracy'] )
+model.compile( loss='categorical_crossentropy', optimizer='adam',metrics=['accuracy'] )
 
 model.fit( 
           X_train_tok,
@@ -73,3 +77,11 @@ model.fit(
           epochs=_EPOCH,
           validation_data=(X_val_tok, y_val)
           )
+
+
+
+y_pred = model.predict( X_test )
+
+print( y_pred )
+
+# print( classification_report(X_test, y_pred ) )
